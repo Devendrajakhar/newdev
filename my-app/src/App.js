34a4dc54356db77f512562b2efdc7be42ae05714
Devendra jakhar <jakhardevendra0@gamil.com>
2025-04-19 23:20:28 +0530

    functions

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,31 +1,39 @@
-import React, { Component } from 'react';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom"; // Make sure you're importing from react-router-dom
-import Navbar from './components/Navbar';
+import React, { useState } from 'react';
+import NavBar from './components/NavBar';
 import News from './components/News';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route exact path='/' element={<News key="general" pageSize={10} country="us" category="general" />} />
-            <Route exact path='/business' element={<News key="business" pageSize={10} country="us" category="business" />} />
-            <Route exact path='/entertainment' element={<News key="entertainment" pageSize={10} country="us" category="entertainment" />} />
-            <Route exact path='/general' element={<News key="general" pageSize={10} country="us" category="general" />} />
-            <Route exact path='/health' element={<News key="health" pageSize={10} country="us" category="health" />} />
-            <Route exact path='/science' element={<News key="science" pageSize={10} country="us" category="science" />} />
-            <Route exact path='/sports' element={<News key="sports" pageSize={10} country="us" category="sports" />} />
-            <Route exact path='/technology' element={<News key="technology" pageSize={10} country="us" category="technology" />} />
-          </Routes>
-        </Router>
-      </div>
-    );
-  }
+const App = () => {
+  const pageSize = 5;
+  // const apiKey = process.env.REACT_APP_NEWS_API;
+  const [progress, setProgress] = useState(0);
+
+  return (
+    <div>
+      <Router>
+        <NavBar />
+        <LoadingBar
+          height={3}
+          color='#f11946'
+          progress={progress}
+          onLoaderFinished={() => setProgress(0)}
+        />
+        <Routes>
+          <Route exact path="/" element={<News setProgress={setProgress}  key="general" pageSize={pageSize} country="us" category="general" />} />
+          <Route exact path="/business" element={<News setProgress={setProgress}  key="business" pageSize={pageSize} country="us" category="business" />} />
+          <Route exact path="/entertainment" element={<News setProgress={setProgress}  key="entertainment" pageSize={pageSize} country="us" category="entertainment" />} />
+          <Route exact path="/general" element={<News setProgress={setProgress}  key="general" pageSize={pageSize} country="us" category="general" />} />
+          <Route exact path="/health" element={<News setProgress={setProgress}  key="health" pageSize={pageSize} country="us" category="health" />} />
+          <Route exact path="/science" element={<News setProgress={setProgress}  key="science" pageSize={pageSize} country="us" category="science" />} />
+          <Route exact path="/sports" element={<News setProgress={setProgress}  key="sports" pageSize={pageSize} country="us" category="sports" />} />
+          <Route exact path="/technology" element={<News setProgress={setProgress}  key="technology" pageSize={pageSize} country="us" category="technology" />} />
+        </Routes>
+      </Router>
+      
+    </div>
+  );
 }
+
+export default App;
--- my-app/src/components/News.js
+++ my-app/src/components/News.js
@@ -1,133 +1,95 @@
-import React, { Component } from 'react'
-import NewsItems from './NewsItems'
+import React, { useEffect, useState } from 'react';
+import NewsItem from './NewsItems';
 import Spinner from './Spinner';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
+const News = (props) => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
 
-export class News extends Component {
-  articles = [
-    {
-      "source": {
-        "id": "the-verge",
-        "name": "The Verge"
-      },
-      "author": "Alex Heath",
-      "title": "Zuckerberg tells court he made WhatsApp, Instagram better",
-      "description": "Towards the end of Mark Zuckerbergâs testimony in a Washington, DC courthouse, a smile flashed across his face.  Metaâs lead attorney, Mark Hansen, had asked the CEO if he was âhappyâ about paying $19 billion for WhatsApp in 2014. Zuckerberg flashed a…",
-      "url": "https://www.theverge.com/policy/650360/mark-zuckerberg-defends-instagram-whatsapp-ftc-meta-antitrust-trial",
-      "urlToImage": "https://platform.theverge.com/wp-content/uploads/sites/2/2025/04/STKS507_FTCxMETA_ANTITRUST_CVIRGINIA_2_A.jpg?quality=90&strip=all&crop=0%2C10.732984293194%2C100%2C78.534031413613&w=1200",
-      "publishedAt": "2025-04-17T00:50:18Z",
-      "content": "During the FTC v. Meta trial, CEO Mark Zuckerberg challenged the governments argument that he bought both apps to snuff them out.\r\nTowards the end of Mark Zuckerbergs testimony in a Washington, DC co… [+4556 chars]"
-    },
-    {
-      "source": {
-        "id": null,
-        "name": "Android Central"
-      },
-      "author": "bradypsnyder@gmail.com (Brady Snyder)",
-      "title": "Android users just dodged a bullet as the CVE cybersecurity tracker stays funded",
-      "description": "You may not know it, but if you have an internet-connected device, you have the CVE program to thank for device security. We all almost lost it.",
-      "url": "https://www.androidcentral.com/apps-software/android-os/android-users-just-dodged-a-bullet-as-the-cve-cybersecurity-tracker-stays-funded",
-      "urlToImage": "https://cdn.mos.cms.futurecdn.net/DsBa2t3vEyHabPemsFwwK8.jpg",
-      "publishedAt": "2025-04-17T00:38:15Z",
-      "content": "Most users of technology don't have to consciously think about security vulnerabilities on their most-used devices, including Android-based products, very often. As long as you update your phone as s… [+5597 chars]"
-    },
-    {
-      "source": {
-        "id": null,
-        "name": "MacRumors"
-      },
-      "author": "Tim Hardwick",
-      "title": "Apple Intelligence Not Available in Meta Apps Like Facebook, Instagram",
-      "description": "Apple has seemingly had one of its most useful AI tools blocked from Meta's apps. Writing Tools, which is an Apple Intelligence feature, is not available to use in Facebook, WhatsApp, Instagram, Threads, and Messenger.\n\n\n\n\n\nApple's Writing Tools include optio…",
-      "url": "https://www.macrumors.com/2025/04/17/apple-intelligence-not-available-in-meta-apps/",
-      "urlToImage": "https://images.macrumors.com/t/BwgXVSeS8z-cmwtblOnMqUi8SY4=/2000x/article-new/2024/07/ios-18-1-writing-tools.jpg",
-      "publishedAt": "2025-04-17T10:26:29Z",
-      "content": "Apple has seemingly had one of its most useful AI tools blocked from Meta's apps. Writing Tools, which is an Apple Intelligence feature, is not available to use in Facebook, WhatsApp, Instagram, Thre… [+1104 chars]"
-    },
-  ]
- static defaultProps = {  
-  country: "us",
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  const updateNews = async () => {
+    props.setProgress(10);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=4299bb2ebb374e2199ae68d0010e56fb&page=1&pageSize=${props.pageSize}`;
+    setLoading(true);
+    let data = await fetch(url);
+    props.setProgress(30);
+    let parsedData = await data.json();
+    props.setProgress(70);
+    setArticles(parsedData.articles || []);
+    setTotalResults(parsedData.totalResults);
+    setLoading(false);
+    props.setProgress(100);
+  };
+
+  useEffect(() => {
+    document.title = `${capitalizeFirstLetter(props.category)} - NewsMonkey`;
+    updateNews();
+    // eslint-disable-next-line
+  }, []);
+
+  const fetchMoreData = async () => {
+    const nextPage = page + 1;
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=4299bb2ebb374e2199ae68d0010e56fb&page=${nextPage}&pageSize=${props.pageSize}`;
+    setPage(nextPage);
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    setArticles((prevArticles) => prevArticles.concat(parsedData.articles || []));
+    setTotalResults(parsedData.totalResults);
+  };
+
+  return (
+    <div className="container">
+      <h1 className="text-center" style={{ margin: '35px 0px', marginTop: '90px' }}>
+        NewsMonkey - Top {capitalizeFirstLetter(props.category)} Headlines
+      </h1>
+      {loading && <Spinner />}
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMoreData}
+        hasMore={articles.length !== totalResults}
+        loader={<Spinner />}
+      >
+        <div className="container">
+          <div className="row">
+            {articles.map((element) => (
+              <div className="col-md-4" key={element.url}>
+                <NewsItem
+                  title={element.title ? element.title : ''}
+                  description={element.description ? element.description : ''}
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                  author={element.author}
+                  date={element.publishedAt}
+                  source={element.source.name}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </InfiniteScroll>
+    </div>
+  );
+};
+
+News.defaultProps = {
+  country: 'in',
   pageSize: 8,
-  category: "general"
-}
-  static propTypes = {
-    country: PropTypes.string,
-    pageSize: PropTypes.number,
-    category: PropTypes.string
-  }
+  category: 'general',
+};
 
-  constructor() {
-    super();
-    this.state = {
-      articles: this.articles,
-      loading: false,
-      page :1
+News.propTypes = {
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string,
+  apiKey: PropTypes.string.isRequired,
+  setProgress: PropTypes.func.isRequired,
+};
 
-    }
-  }
-
-  async componentDidMount() {
-   let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d144c972e0c247249f10d97c8adfb4cb&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-   this.setState({ loading: true })
-    let data = await fetch(url);
-    let parsedData = await data.json()
-    console.log(parsedData);
-    this.setState({ articles: parsedData.articles, totalresults: parsedData.totalResults, loading: false  });
-
-  }
-  handlenextclick = async () => {
-  if(this.state.page+1>Math.ceil(this.state.totalresults/this.props.pageSize)){
-   return;
-  }
-  else{
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d144c972e0c247249f10d97c8adfb4cb&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true })
-    let data = await fetch(url);
-    let parsedData = await data.json()
-    
-    this.setState({
-      articles: parsedData.articles,
-      page: this.state.page + 1,
-      loading: false
-    })
-    console.log("click next")
-  }
-  }
-  handlepreviouclick = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d144c972e0c247249f10d97c8adfb4cb&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true })
-    let data = await fetch(url);
-    let parsedData = await data.json()
-    this.setState({
-      articles: parsedData.articles,
-      page: this.state.page - 1,
-      loading: false
-    })
-    console.log("click prec")
-  }
-  render() {
-    return (
-      <div className="container">
-        <h2 className='text-center' >New - Top Headlines</h2>
-       
-        <div className="text-center">{this.state.loading&&<Spinner/>}</div>
-        
-        <div className='row'>
-          {Array.isArray(this.state.articles)&&this.state.articles.map((element) => {
-            return (
-              <div className="col-md-4 mt-2" key={element.url}>
-                <NewsItems title={element.title} description={element.description} imageurl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.name}/>
-              </div>)
-          })}
-        </div>
-        <div className="container d-flex justify-content-between">
-          <button type="button" disabled={this.state.page <= 1} className="btn btn-dark mt-2 mb-2" onClick={this.handlepreviouclick}> &larr; previous</button>
-          <button type="button" className="btn btn-dark  mt-2 mb-2" onClick={this.handlenextclick} disabled={this.state.page + 1 > Math.ceil(this.state.totalresults / this.props.pageSize)}>next &rarr;</button>
-        </div>
-      </div>
-    )
-  }
-}
-
-export default News
+export default News;
